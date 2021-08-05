81d999062b3c4fd07b07cce09db5a74a896b19ea
Bruno Gomes <icestonebruno@gmail.com>
2021-08-05 11:01:51 -0300

    aula 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,46 @@
+import { useHistory } from 'react-router-dom'
+import {firebase, auth} from '../services/firebase'
 import illustrationImg from '../assets//images/illustration.svg'
 import logoImg from '../assets//images/logo.svg'
 import googleIconImg from '../assets//images/google-icon.svg'
 
+
+
 import '../styles/auth.scss'
+import { Button } from '../components/Button'
 
 export function Home(){
+  const history = useHistory()
+
+  function handleCreateRoom(){
+    history.push('/rooms/new')
+  }
+
+
   return(
     <div id='page-auth'>
       <aside>
         <img src={illustrationImg} alt="Simbolizando perguntas e respostas" />
-        <strong>Cire salar de Q&amp;A ao-vivo</strong>
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-container">
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Entre com o Google" />
-            Cire sua sala com o Google
+            Crie sua sala com o Google
           </button>
           
-          <div>
+          <div className="separator">
             ou entre em uma sala
           </div>
 
           <form>
             <input type="text" placeholder="Digite o código da sala" />
-            <button type='submit'>
+            <Button type='submit'>
               Entrar na sala
-            </button>
+            </Button>
           </form>
         </div>
       </main>
